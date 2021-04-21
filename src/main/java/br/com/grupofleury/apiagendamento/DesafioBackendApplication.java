610cf51c788c2753fae88d31858b19e4679fcf34
MardonioEng <mardoniorodrigues11@gmail.com>
2021-04-21 16:08:36 -0300

    Entidade Cliente

--- src/main/java/br/com/grupofleury/apiagendamento/DesafioBackendApplication.java
+++ src/main/java/br/com/grupofleury/apiagendamento/DesafioBackendApplication.java
@@ -2,9 +2,8 @@ package br.com.grupofleury.apiagendamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class DesafioBackendApplication {
 
 	public static void main(String[] args) {
